3ae5daf6970d94a8c5f8adc9809594060a0ded38
Malky <malkyseitler@gmail.com>
2023-06-08 12:41:33 -0400

    added trips

--- src/components/Trips.jsx
+++ src/components/Trips.jsx
@@ -1,64 +1,137 @@
 export const Activities = [
-  {
-    name: "Shore Lanes",
-    address: "701 NJ-35, Neptune City, NJ 07753",
-    website: "https://www.shorelanesnj.com/",
-    description:
-      "A bowling ally open until 11pm every night. It's located in Neptune which is not far from Lakewood but it is still a drive away giving you the feeling that you really went out.",
-    closes: 11,
-    categoryId: 2,
-  },
-  {
-    name: "Ocean Lanes",
-    address: "2085 Lanes Mill Rd. Lakewood, NJ 08701",
-    website: "https://oceanlanes.com/",
-    description:
-      "A bowling ally open until 12am every night. Located off Ocean avenue its a great destination if you don't want to travel far. It gets full during Bein hazmanim and other times as well so be prepared to meet some friends!",
-    closes: 12,
-    categoryId: 2,
-  },
-  {
-    name: "Finnegans Lanes",
-    address: "101 Locust St, Lakewood, NJ 08701",
-    website: "https://www.finnigansfun.com/",
-    description:
-      "A bowling ally open until 12am every night. Located down Rt 9 South this bowling location is generally less busy than others in the area, however you can still expect to run into others durin Bein Hazmanim and off shabbasim",
-    closes: 12,
-    categoryId: 2,
-  },
-  {
-    name: "Howell Lanes",
-    address: "1002 US-9, Howell Township, NJ 07731",
-    website: "https://www.howelllanes.com/",
-    description:
-      "A bowling ally open until 12am every night. Located down Rt 9 North this bowling location is generally less busy than others in the area, however you can still expect to run into others durin Bein Hazmanim and off shabbasim",
-    closes: 12,
-    categoryId: 2,
-  },
-  {
-    name: "Top Golf - Edison",
-    address: "1013 US-1, Edison, NJ 08817",
-    website: "https://topgolf.com/us/edison/",
-    description:
-      "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead as this location especially can get very crowded",
-    closes: 1,
-    categoryId: 1,
-  },
-  {
-    name: "Top Golf - Mount Laurel",
-    address: "104 Centerton Rd, Mt Laurel Township, NJ 08054",
-    website: "https://topgolf.com/us/mount-laurel/",
-    description:
-      "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead but this location is generally a bit quieter than Edison and usually has better time slots available",
-    closes: 1,
-    categoryId: 1,
-  },
+    {
+        name: "Shore Lanes",
+        address: "701 NJ-35, Neptune City, NJ 07753",
+        website: "https://www.shorelanesnj.com/",
+        description:
+            "A bowling ally open until 11pm every night. It's located in Neptune which is not far from Lakewood but it is still a drive away giving you the feeling that you really went out.",
+        closes: 11,
+        categoryId: 2,
+    },
+    {
+        name: "Ocean Lanes",
+        address: "2085 Lanes Mill Rd. Lakewood, NJ 08701",
+        website: "https://oceanlanes.com/",
+        description:
+            "A bowling ally open until 12am every night. Located off Ocean avenue its a great destination if you don't want to travel far. It gets full during Bein hazmanim and other times as well so be prepared to meet some friends!",
+        closes: 12,
+        categoryId: 2,
+    },
+    {
+        name: "Finnegans Lanes",
+        address: "101 Locust St, Lakewood, NJ 08701",
+        website: "https://www.finnigansfun.com/",
+        description:
+            "A bowling ally open until 12am every night. Located down Rt 9 South this bowling location is generally less busy than others in the area, however you can still expect to run into others durin Bein Hazmanim and off shabbasim",
+        closes: 12,
+        categoryId: 2,
+    },
+    {
+        name: "Howell Lanes",
+        address: "1002 US-9, Howell Township, NJ 07731",
+        website: "https://www.howelllanes.com/",
+        description:
+            "A bowling ally open until 12am every night. Located down Rt 9 North this bowling location is generally less busy than others in the area, however you can still expect to run into others durin Bein Hazmanim and off shabbasim",
+        closes: 12,
+        categoryId: 2,
+    },
+    {
+        name: "Top Golf - Edison",
+        address: "1013 US-1, Edison, NJ 08817",
+        website: "https://topgolf.com/us/edison/",
+        description:
+            "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead as this location especially can get very crowded",
+        closes: 1,
+        categoryId: 1,
+    },
+    {
+        name: "Top Golf - Mount Laurel",
+        address: "104 Centerton Rd, Mt Laurel Township, NJ 08054",
+        website: "https://topgolf.com/us/mount-laurel/",
+        description:
+            "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead but this location is generally a bit quieter than Edison and usually has better time slots available",
+        closes: 1,
+        categoryId: 1,
+    },
+    //DIDN'T DO DESCRIPTION
+    {
+        name: "Dave and Busters - Woodbridge Mall",
+        address: "274 Woodbridge Center Dr, Woodbridge Township, NJ 07095",
+        website: "https://www.daveandbusters.com/us/en/about/locations/woodbridge-middlesex",
+        description:
+            "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead but this location is generally a bit quieter than Edison and usually has better time slots available",
+        closes: 12,
+        categoryId: 1,
+    },
+    {
+        name: "Jenkinson's Boardwalk",
+        address: "300 Ocean Ave N, Point Pleasant Beach, NJ 08742",
+        website: "https://jenkinsons.com/",
+        description:
+            "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead but this location is generally a bit quieter than Edison and usually has better time slots available",
+        closes: 12,
+        categoryId: 2,
+    },
+    {
+        name: "Point Pleasant Boardwalk",
+        address: "Point pleasant beach",
+        website: "https://pointpleasantbeach.org/",
+        description:
+            "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead but this location is generally a bit quieter than Edison and usually has better time slots available",
+        closes: 12,
+        categoryId: 2,
+    },
+    {
+        name: "Sprinkles - South",
+        address: "1091 River Ave, Lakewood, NJ 08701",
+        website: "https://sprinkleskosher.com/lakewood-todd-plaza/",
+        description:
+            "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead but this location is generally a bit quieter than Edison and usually has better time slots available",
+        closes: 10,
+        categoryId: 2,
+    },
+    {
+        name: "Sprinkles - North",
+        address: "1700 Madison Ave #12, Lakewood, NJ 08701",
+        website: "https://sprinkleskosher.com/lakewood-gg-plaza/",
+        description:
+            "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead but this location is generally a bit quieter than Edison and usually has better time slots available",
+        closes: 10,
+        categoryId: 2,
+    },
+    {
+        name: "Cream Gelateria",
+        address: "12 America Ave, Lakewood, NJ 08701",
+        website: "https://creamgelateria.com/",
+        description:
+            "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead but this location is generally a bit quieter than Edison and usually has better time slots available",
+        closes: 1130,
+        categoryId: 2,
+    },
+    {
+        name: "Avery & Jay's Handcrafted Ice Cream",
+        address: "1135 E Veterans Hwy, Jackson Township, NJ 08527",
+        website: "https://www.averyandjays.com/",
+        description:
+            "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead but this location is generally a bit quieter than Edison and usually has better time slots available",
+        closes: 11,
+        categoryId: 2,
+    },
+    {
+        name: "Rita's - Lakewood",
+        address: "681 River Ave, Lakewood, NJ 08701",
+        website: "https://www.ritasice.com/location/ritas-of-lakewood-nj/",
+        description:
+            "Top golf is a great spot for when you want a night. It is set up to be a date night attraction with comfortable couches at each station and drinks for purchase. The atmostphere is adult like and relaxed. It is reccommended to book ahead but this location is generally a bit quieter than Edison and usually has better time slots available",
+        closes: 11,
+        categoryId: 2,
+    },
 ];
 
+
 export const Categories = [
-  { name: "day-trips", id: 0 },
-  { name: "date-night", id: 1 },
-  { name: "quick-trips", id: 2 },
-  { name: "at-home", id: 3 },
-  { name: "restaurants", id: 4 },
+    { name: "day-trips", id: 0 },
+    { name: "date-night", id: 1 },
+    { name: "quick-trips", id: 2 },
+    { name: "at-home", id: 3 },
 ];
