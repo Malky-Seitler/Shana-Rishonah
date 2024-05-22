ee4ae09d5c6c799909745dac5edf765b7c23e538
Malky Pick <malkyseitler@gmail.com>
2024-05-22 10:33:03 -0400

    remove absolute

--- src/components/FilteredPage.jsx
+++ src/components/FilteredPage.jsx
@@ -5,16 +5,20 @@ import { useParams } from "react-router-dom";
 import LightAd from "./ads/lightsad.png";
 import { useActivityContext } from "../ActivityContext";
 import { Container } from "reactstrap";
+import { Loader } from "semantic-ui-react";
 
 const FilteredPage = () => {
   const { type } = useParams();
+  const [loading, setLoading] = useState(true);
   const { getListToUse, showSearch, filteredList, setFilteredList } =
     useActivityContext();
 
   const getList = async () => {
+    setLoading(true);
     await getListToUse(type).then((data) => {
       setFilteredList(data);
     });
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -26,11 +30,13 @@ const FilteredPage = () => {
   if (filteredList?.length === 0) {
     return;
   }
-
+  if (loading) {
+    return <Loader active />;
+  }
   return (
     <div>
       <div>
-        <img src={LightAd} style={{ width: 350, position: "absolute" }} />
+        <img src={LightAd} />
       </div>
       <Container style={{ paddingRight: 24, paddingLeft: 24 }}>
         <div>
