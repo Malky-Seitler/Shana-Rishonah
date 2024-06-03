bbd71ac84de48114666a2b3f0344c2a64dd07946
Malky Pick <malkyseitler@gmail.com>
2024-06-03 14:54:16 -0400

    add resources

---
+++ src/components/Pages/Resources/resources.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Resources = [
+  {
+    name: "Ani L'dodi Helpline",
+    number: "646-746-4343",
+    hours: "Monday and Wednesday 8:30-10:00pm. Tuesday and Thursday 9:30-11am",
+    website: "https://aniledodihelpline.org/",
+  },
+  {
+    name: "Tahareinu Hotline",
+    number: "888-565-3628",
+    website: "https://tahareinu.com/hotline/",
+    hours: "8:00am - 4:00pm",
+  {
+    name: "ATime Hug (support for pregnancy loss)",
+    number: "718-686-8912 ext. 113",
+    website: "https://www.atime.org/services/hug/",
+  },
+  {
+    name: "Knafayim (support for pregnancy loss)",
+    number: "Number varies based on location",
+    website: "https://knafayimwings.org/",
+    hours: "24/7",
+  },
+];
